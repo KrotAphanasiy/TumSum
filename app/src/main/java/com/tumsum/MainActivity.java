d1de18568b8808091d1e2e40f89cd81ac2a521fa
Михаил Попиков <mihail.popikov2002@gmail.com>
2022-04-05 23:10:17 +0300

    in progress

--- app/src/main/java/com/tumsum/MainActivity.java
+++ app/src/main/java/com/tumsum/MainActivity.java
@@ -2,7 +2,9 @@ package com.tumsum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_page);
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
 }
